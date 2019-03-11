e06ae8fe04a10f3bbb5f538c793bef257c513c93
Wu Yanjiang <junglew@qq.com>
2019-03-11 23:18:33 +0800

    get post中文乱码问题解决

--- WEB15/src/com/itheima/register/RegisterServlet.java
+++ WEB15/src/com/itheima/register/RegisterServlet.java
@@ -31,6 +31,17 @@ public class RegisterServlet extends HttpServlet {
 //        user.setUsername(username);
         //...
 
+        //只适合post方式的表单提交的中文乱码
+        //request.setCharacterEncoding("UTF-8");
+        //服务器默认查的是ISO8859-
+
+        //get方式乱码解决  也适用与post
+        String username = request.getParameter("username");
+        //先用ISO8859-1编码，再使用utf-8解码
+        username = new String(username.getBytes("iso8859-1"), "utf-8");
+        //表单一多就麻烦，不适用
+
+
 
         //使用BeanUtils进行自动映射封装
         //工作原理：将一个map中的数据，根据key与实体的属性的对应关系封装
@@ -38,6 +49,7 @@ public class RegisterServlet extends HttpServlet {
         Map<String, String[]> properties = request.getParameterMap();
         User user = new User();
         try {
+            //这里有一个异常不知道怎么解决
             BeanUtils.populate(user, properties);
         } catch (IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
@@ -55,13 +67,13 @@ public class RegisterServlet extends HttpServlet {
         }
 
         //注册成功跳转登录页面
-        response.sendRedirect(request.getContextPath() + "/lgin.jsp");
+        response.sendRedirect(request.getContextPath() + "/login.jsp");
 
 
     }
 
     //注册的方法
-    public void register(User user) throws SQLException {
+    private void register(User user) throws SQLException {
         //操作数据库
         QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
         String sql = "insert into user values(?,?,?,?,?,?,?,?,?,?)";
