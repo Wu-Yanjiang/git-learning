bd9c82afb025dbc250fb81ca191bb2fba367aa98
Wu Yanjiang <junglew@qq.com>
2019-04-27 14:24:46 +0800

    leetcode

---
+++ leetcode/src/cn/wyj/solution/MaximalSquare.java
@@ -0,0 +1,23 @@
+package cn.wyj.solution;
+
+import static java.lang.Math.min;
+
+public class MaximalSquare {
+    public int maximalSquare(char[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] dp = new int[row][col];
+        int max = 0;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == 0 || j == 0) dp[i][j] = matrix[i][j] - '0';
+                else if (matrix[i][j] == '1') {
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                }
+                max = max(dp[i][j], max);
+            }
+        }
+        return max*max;
+    }
+}
