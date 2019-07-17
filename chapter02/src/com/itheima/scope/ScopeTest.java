939c5d766da7ab919602bdd12175ed9b4d3dbbda
Wu Yanjiang <junglew@qq.com>
2019-07-17 23:50:38 +0800

    bean assemble

---
+++ chapter02/src/com/itheima/scope/ScopeTest.java
@@ -0,0 +1,14 @@
+package com.itheima.scope;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ScopeTest {
+    public static void main(String[] args) {
+        String xmlPath = "com/itheima/scope/beans4.xml";
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(xmlPath);
+//        输出获得实例
+        System.out.println(applicationContext.getBean("scope"));
+        System.out.println(applicationContext.getBean("scope"));
+    }
+}
